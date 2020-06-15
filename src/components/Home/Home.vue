aceb3c1d65776ed6e7ecf435d562ec52abc6ab63
James Abraham Olamide <horlamidex1@gmail.com>
2020-06-15 17:37:38 +0000

    init commit

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <compHeader/>
+    </div>
+</template>
+<script>
+import compHeader from '../Header_Footer/Header.vue';
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    components:{
+        compHeader
+    }
+}
+</script>
